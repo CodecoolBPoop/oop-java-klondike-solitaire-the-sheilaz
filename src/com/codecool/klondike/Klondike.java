21974a2a98ceb98bb8aa07ea25fbd035660dffc9
myrelin <anna.iklody@gmail.com>
2018-11-28 16:15:43 +0100

    unused import removed from klondike.java

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -3,11 +3,9 @@ package com.codecool.klondike;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
+
 
 
 
